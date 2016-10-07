8d6c8206b20b4178c993927212a35e4a32b3caf7
darkua <sergioveiga@gmail.com>
2016-10-07 19:24:33 +0200

    add consumer group

--- main.go
+++ main.go
@@ -9,14 +9,16 @@ import (
 	"strings"
 
 	"github.com/Shopify/sarama"
+	log "github.com/Sirupsen/logrus"
 	"github.com/fatih/color"
 	"github.com/larskluge/babl-server/kafka"
 )
 
 //Broker url
 const (
-	Broker    = "v5.babl.sh:9092"
-	Topic     = "logs.raw"
+	Broker = "v5.babl.sh:9092"
+	// Topic  = "logs.raw"
+	Topic     = "babl.larskluge.RenderWebsite.IO"
 	Partition = 0
 	LastN     = 1000
 	Cols      = 3
@@ -67,77 +69,137 @@ func main() {
 		return
 	}
 
-	client := *kafka.NewClient([]string{Broker}, "babl-admin", true)
-	defer client.Close()
+	// client := *kafka.NewClient([]string{Broker}, "babl-admin", true)
+	// defer client.Close()
 
-	consumer, err := sarama.NewConsumerFromClient(client)
-	check(err)
-	defer consumer.Close()
+	// consumer, err := sarama.NewConsumerFromClient(client)
+	// check(err)
+	// defer consumer.Close()
 
-	offsetNewest, err := client.GetOffset(Topic, Partition, sarama.OffsetNewest)
-	check(err)
-	offsetOldest, err := client.GetOffset(Topic, Partition, sarama.OffsetOldest)
-	check(err)
+	// offsetNewest, err := client.GetOffset(Topic, Partition, sarama.OffsetNewest)
+	// check(err)
+	// offsetOldest, err := client.GetOffset(Topic, Partition, sarama.OffsetOldest)
+	// check(err)
 
-	offset := offsetNewest - LastN
-	if offset < 0 || offset < offsetOldest {
-		offset = offsetOldest
-	}
+	// offset := offsetNewest - LastN
+	// if offset < 0 || offset < offsetOldest {
+	// 	offset = offsetOldest
+	// }
 
-	cp, err := consumer.ConsumePartition(Topic, Partition, offset)
-	check(err)
-	defer cp.Close()
+	// cp, err := consumer.ConsumePartition(Topic, Partition, offset)
+	// check(err)
+	// defer cp.Close()
 
-	for msg := range cp.Messages() {
-		var m Msg
-		err := json.Unmarshal(msg.Value, &m)
-		check(err)
-
-		// MESSAGE can be a string or []byte which represents a string; bug in journald/kafka-manager somehow
-		var s string
-		err = json.Unmarshal(m.MessageRaw, &s)
-		if err == nil {
-			m.Message = s
-		} else {
-			var n []byte
-			err = json.Unmarshal(m.MessageRaw, &n)
-			check(err)
-			m.Message = string(n)
-		}
-
-		level := logLevel(m)
-		switch level {
-		case 'E':
-			color.Set(color.FgRed)
-		case 'W':
-			color.Set(color.FgYellow)
-		}
-		log(m.Hostname, AppName(m), m.Message)
-		color.Unset()
-	}
+	// for msg := range cp.Messages() {
+	// 	// parseRaw(msg)
+	// 	// parseTopic(msg)
+	// }
+	// client := *kafka.NewClient([]string{Broker}, "babl-admin", true)
+	clientgroup := kafka.NewClientGroup([]string{Broker}, "babl-admin", true)
+	defer (*clientgroup).Close()
+	parseGroup(clientgroup, []Topic)
 }
 
-func log(entries ...string) {
-	if Cols != len(entries) {
-		panic("Adjust Cols")
-	}
-	for i, entry := range entries {
-		w := 0
+// func log(entries ...string) {
+// 	if Cols != len(entries) {
+// 		panic("Adjust Cols")
+// 	}
+// 	for i, entry := range entries {
+// 		w := 0
 
-		// do not adjust col width for last col
-		if i < Cols-1 {
-			if len(entry) > ColW[i] {
-				ColW[i] = len(entry)
+// 		// do not adjust col width for last col
+// 		if i < Cols-1 {
+// 			if len(entry) > ColW[i] {
+// 				ColW[i] = len(entry)
+// 			}
+// 			w = ColW[i] + Padding
+// 		}
+
+// 		fmt.Printf("%-"+strconv.Itoa(w)+"s", entry)
+// 	}
+// 	fmt.Println()
+// }
+
+func parseGroup(clientgroup *cluster.Client, topics []string) {
+	ch := make(chan *kafka.ConsumerData)
+	go kafka.ConsumeGroup(clientgroup, topics, ch)
+
+	for {
+		log.WithFields(log.Fields{"topics": topics}).Debug("Work")
+
+		data, _ := <-ch
+		log.WithFields(log.Fields{"key": data.Key}).Debug("Request recieved in module's topic/group")
+
+		rid := SplitLast(data.Key, ".")
+		async := false
+		res := "error"
+		var msg []byte
+		method := SplitLast(data.Topic, ".")
+		switch method {
+		case "IO":
+			in := &pbm.BinRequest{}
+			err := proto.Unmarshal(data.Value, in)
+			Check(err)
+			_, async = in.Env["BABL_ASYNC"]
+			delete(in.Env, "BABL_ASYNC") // worker needs to process job synchronously
+			if len(in.Env) == 0 {
+				in.Env = map[string]string{}
 			}
-			w = ColW[i] + Padding
+			in.Env["BABL_RID"] = rid
+			out, err := IO(in, MaxKafkaMessageSize)
+			Check(err)
+			if out.Exitcode == 0 {
+				res = "success"
+			}
+			msg, err = proto.Marshal(out)
+			Check(err)
+		case "Ping":
+			in := &pbm.Empty{}
+			err := proto.Unmarshal(data.Value, in)
+			Check(err)
+			out, err := Ping(in)
+			Check(err)
+			res = "success"
+			msg, err = proto.Marshal(out)
+			Check(err)
 		}
-
-		fmt.Printf("%-"+strconv.Itoa(w)+"s", entry)
+		data.Processed <- res
 	}
-	fmt.Println()
 }
 
-//AppName...
+func parseTopic(msg *sarama.ConsumerMessage) {
+	data := *kafka.ConsumerData{Key: string(msg.Key), Value: msg.Value}
+	logrus.WithFields(logrus.Fields{"topic": Topic, "partition": msg.Partition, "offset": msg.Offset, "key": data.Key, "value": string(data.Value), "value size": len(data.Value), "rid": data.Key}).Info("New Message Received")
+}
+
+func parseRaw(msg *sarama.ConsumerMessage) {
+	var m Msg
+	err := json.Unmarshal(msg.Value, &m)
+	check(err)
+
+	// MESSAGE can be a string or []byte which represents a string; bug in journald/kafka-manager somehow
+	var s string
+	err = json.Unmarshal(m.MessageRaw, &s)
+	if err == nil {
+		m.Message = s
+	} else {
+		var n []byte
+		err = json.Unmarshal(m.MessageRaw, &n)
+		check(err)
+		m.Message = string(n)
+	}
+	level := logLevel(m)
+	switch level {
+	case 'E':
+		color.Set(color.FgRed)
+	case 'W':
+		color.Set(color.FgYellow)
+	}
+	log(m.Hostname, AppName(m), m.Message)
+	color.Unset()
+}
+
+//AppName extraction func...
 func AppName(m Msg) string {
 	if m.SyslogIdentifier != "" {
 		return m.SyslogIdentifier
