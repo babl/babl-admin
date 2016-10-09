9f98f85b63185faaf4b7ddc63a1a84b122055a53
Lars Kluge <l@larskluge.com>
2016-10-09 12:28:39 +0200

    Use const

--- deploy.go
+++ deploy.go
@@ -5,8 +5,11 @@ import (
 	"strings"
 )
 
+const (
+	deployTemplate = "docker service create --name %s --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=v5.babl.sh:9092 -e BABL_STORAGE=v5.babl.sh:4443 registry.babl.sh/%s:%s\n"
+)
+
 func Deploy(module, version string, mem int) {
 	serviceName := strings.Replace(module, "/", "--", 1)
-	fmt.Printf("docker service create --name %s --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=v5.babl.sh:9092 -e BABL_STORAGE=v5.babl.sh:4443 registry.babl.sh/%s:%s\n",
-		serviceName, mem, mem, module, version, module, version)
+	fmt.Printf(deployTemplate, serviceName, mem, mem, module, version, module, version)
 }
