69925a2e1b0056748c5c54be8d582e84d6e6cab4
Lars Kluge <l@larskluge.com>
2016-10-12 16:35:46 +0200

    Update deploy config

--- deploy.go
+++ deploy.go
@@ -6,10 +6,10 @@ import (
 )
 
 const (
-	deployTemplate = "docker service create --name %s --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=v5.babl.sh:9092 -e BABL_STORAGE=v5.babl.sh:4443 registry.babl.sh/%s:%s\n"
+	deployTemplate = "docker service create --name %s --label=module --network=modules -p 4444 --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=$CLUSTER_ADDR:9092 -e BABL_STORAGE=$CLUSTER_ADDR:4443 -e BABL_ENDPOINT=$CLUSTER_ADDR:4444 registry.babl.sh/%s:%s"
 )
 
 func Deploy(module, version string, mem int) {
 	serviceName := strings.Replace(module, "/", "--", 1)
-	fmt.Printf(deployTemplate, serviceName, mem, mem, module, version, module, version)
+	fmt.Printf(deployTemplate+"\n", serviceName, mem, mem, module, version, module, version)
 }
