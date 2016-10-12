e1486947c50acbb877ba13e6519f53cdb0347602
darkua <sergioveiga@gmail.com>
2016-10-12 23:02:22 +0200

    separate dir

---
+++ raw.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/Shopify/sarama"
+	"github.com/fatih/color"
+	"github.com/larskluge/babl-server/kafka"
+	"regexp"
+	"strings"
+)
+
+//Warning log level
+
+//Broker url
+const (
+	Topic     = "logs.raw"
+	Partition = 0
+	LastN     = 1000
+	Cols      = 3
+	Padding   = 1
+)
+
+var (
+	Warning = regexp.MustCompile("(?i)warn|overloaded")
+	Error   = regexp.MustCompile("(?i)error|panic|fail|killed|exception")
+
+	ColW = make([]int, Cols-1)
+)
+
+//Msg structure
+type Msg struct {
+	Hostname         string          `json:"_HOSTNAME"`
+	SystemdUnit      string          `json:"_SYSTEMD_UNIT"`
+	SyslogIdentifier string          `json:"SYSLOG_IDENTIFIER"`
+	ContainerName    string          `json:"CONTAINER_NAME"`
+	MessageRaw       json.RawMessage `json:"MESSAGE"`
+	Message          string
+}
+
+func ParseRaw() {
+	client := *kafka.NewClient([]string{Broker}, "babl-admin", true)
+	defer client.Close()
+
+	consumer, err := sarama.NewConsumerFromClient(client)
+	check(err)
+	defer consumer.Close()
+
+	offsetNewest, err := client.GetOffset(Topic, Partition, sarama.OffsetNewest)
+	check(err)
+	offsetOldest, err := client.GetOffset(Topic, Partition, sarama.OffsetOldest)
+	check(err)
+
+	offset := offsetNewest - LastN
+	if offset < 0 || offset < offsetOldest {
+		offset = offsetOldest
+	}
+
+	cp, err := consumer.ConsumePartition(Topic, Partition, offset)
+	check(err)
+	defer cp.Close()
+
+	for msg := range cp.Messages() {
+		parseMessage(msg)
+	}
+}
+func log(entries ...string) {
+	if Cols != len(entries) {
+		panic("Adjust Cols")
+	}
+	for i, entry := range entries {
+		w := 0
+
+		// do not adjust col width for last col
+		if i < Cols-1 {
+			if len(entry) > ColW[i] {
+				ColW[i] = len(entry)
+			}
+			w = ColW[i] + Padding
+		}
+
+		fmt.Printf("%-"+strconv.Itoa(w)+"s", entry)
+	}
+	fmt.Println()
+}
+
+func parseMessage(msg *sarama.ConsumerMessage) {
+	var m Msg
+	err := json.Unmarshal(msg.Value, &m)
+	check(err)
+
+	// MESSAGE can be a string or []byte which represents a string; bug in journald/kafka-manager somehow
+	var s string
+	err = json.Unmarshal(m.MessageRaw, &s)
+	if err == nil {
+		m.Message = s
+	} else {
+		var n []byte
+		err = json.Unmarshal(m.MessageRaw, &n)
+		check(err)
+		m.Message = string(n)
+	}
+	level := logLevel(m)
+	switch level {
+	case 'E':
+		color.Set(color.FgRed)
+	case 'W':
+		color.Set(color.FgYellow)
+	}
+	log(m.Hostname, AppName(m), m.Message)
+	color.Unset()
+}
+
+func AppName(m Msg) string {
+	if m.SyslogIdentifier != "" {
+		return m.SyslogIdentifier
+	}
+	app := strings.TrimSuffix(m.SystemdUnit, ".service")
+	if app == "docker" {
+		app = m.ContainerName
+
+		// strip instance id
+		r := regexp.MustCompile(`^([^\.]+\.\d+)\.\w+$`)
+		matches := r.FindStringSubmatch(app)
+		if matches != nil {
+			app = matches[1]
+		}
+	} else if strings.HasPrefix(app, "sshd@") {
+		app = "sshd"
+	}
+	return app
+}
+
+func logLevel(m Msg) rune {
+	if Error.MatchString(m.Message) {
+		return 'E'
+	} else if Warning.MatchString(m.Message) {
+		return 'W'
+	} else {
+		return 'I'
+	}
+}
---
+++ topic.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"regexp"
+	// "strconv"
+	"strings"
+
+	// "github.com/Shopify/sarama"
+	log "github.com/Sirupsen/logrus"
+	// "github.com/fatih/color"
+	"github.com/golang/protobuf/proto"
+	"github.com/larskluge/babl-server/kafka"
+	. "github.com/larskluge/babl/bablutils"
+	pbm "github.com/larskluge/babl/protobuf/messages"
+	"gopkg.in/bsm/sarama-cluster.v2"
+)
+
+func ParseTopic(Topic string) {
+	clientgroup := kafka.NewClientGroup([]string{Broker}, "babl-admin", true)
+	defer (*clientgroup).Close()
+	//[]string{"babl.larskluge.StringUpcase.IO", "babl.larskluge.StringUpcase.Ping"}
+	parseGroup(clientgroup, []string{Topic})
+}
+
+func ConsumeGroup(client *cluster.Client, topics []string, ch chan *kafka.ConsumerData) {
+	group := "babl-admin-inspector"
+	log.WithFields(log.Fields{"topics": topics, "group": group, "offset": "newest"}).Info("Consuming Groups")
+
+	consumer, err := cluster.NewConsumerFromClient(client, group, topics)
+	Check(err)
+	defer consumer.Close()
+
+	go consumeErrors(consumer)
+	go consumeNotifications(consumer)
+
+	for msg := range consumer.Messages() {
+		data := kafka.ConsumerData{Topic: msg.Topic, Key: string(msg.Key), Value: msg.Value, Processed: make(chan string, 1)}
+		rid := SplitLast(data.Key, ".")
+		log.WithFields(log.Fields{"topics": topics, "group": group, "partition": msg.Partition, "offset": msg.Offset, "key": data.Key, "value size": len(data.Value), "rid": rid}).Info("New Group Message Received")
+		ch <- &data
+		metadata := <-data.Processed
+		consumer.MarkOffset(msg, metadata)
+	}
+	log.Println("ConsumerGroups: Done consuming topic/groups", topics)
+}
+
+func consumeErrors(consumer *cluster.Consumer) {
+	for err := range consumer.Errors() {
+		log.WithFields(log.Fields{"error": err.Error()}).Warn("ConsumeGroup: Error")
+	}
+}
+
+func consumeNotifications(consumer *cluster.Consumer) {
+	for note := range consumer.Notifications() {
+		log.WithFields(log.Fields{"rebalanced": note}).Info("ConsumeGroup: Notification")
+	}
+}
+
+func parseGroup(clientgroup *cluster.Client, topics []string) {
+	ch := make(chan *kafka.ConsumerData)
+	go ConsumeGroup(clientgroup, topics, ch)
+
+	for {
+		log.WithFields(log.Fields{"topics": topics}).Debug("Work")
+
+		data, _ := <-ch
+		log.WithFields(log.Fields{"key": data.Key}).Debug("Request recieved in module's topic/group")
+
+		rid := SplitLast(data.Key, ".")
+
+		res := "error"
+		method := SplitLast(data.Topic, ".")
+		switch method {
+		case "IO":
+			in := &pbm.BinRequest{}
+			err := proto.Unmarshal(data.Value, in)
+			check(err)
+			log.WithFields(log.Fields{"rid": rid, "data": in}).Debug("IO:")
+			res = "success"
+		case "Ping":
+			in := &pbm.Empty{}
+			err := proto.Unmarshal(data.Value, in)
+			check(err)
+			log.WithFields(log.Fields{"rid": rid, "data": in}).Debug("PING:")
+			res = "success"
+		}
+		data.Processed <- res
+	}
+}
