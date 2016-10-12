a2b8b12580f1774390b1536686caca56a3d074e4
Lars Kluge <l@larskluge.com>
2016-10-12 17:30:26 +0200

    Fix port number for v5

--- deploy.go
+++ deploy.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	deployTemplate = "docker service create --name %s --label=module --network=modules -p 4444 --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=$CLUSTER_ADDR:9092 -e BABL_STORAGE=$CLUSTER_ADDR:4443 -e BABL_ENDPOINT=$CLUSTER_ADDR:4444 registry.babl.sh/%s:%s"
+	deployTemplate = "docker service create --name %s --label=module --network=modules -p 4444 --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=$CLUSTER_ADDR:9092 -e BABL_STORAGE=$CLUSTER_ADDR:4443 -e BABL_ENDPOINT=$CLUSTER_ADDR:4445 registry.babl.sh/%s:%s"
 )
 
 func Deploy(module, version string, mem int) {
