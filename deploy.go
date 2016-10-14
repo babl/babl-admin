843a5d0230315a9e760629e05ae86b06d9edaa9f
Lars Kluge <l@larskluge.com>
2016-10-14 17:10:04 +0200

    Use cluster config for deploy as well

--- deploy.go
+++ deploy.go
@@ -6,10 +6,10 @@ import (
 )
 
 const (
-	deployTemplate = "docker service create --name %s --label=module --network=modules -p 4444 --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=$CLUSTER_ADDR:9092 -e BABL_STORAGE=$CLUSTER_ADDR:4443 -e BABL_ENDPOINT=$CLUSTER_ADDR:4445 registry.babl.sh/%s:%s"
+	deployTemplate = "docker service create --name %s --label=module --network=modules -p 4444 --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=%s:9092 -e BABL_STORAGE=%s:4443 -e BABL_ENDPOINT=%s:4445 registry.babl.sh/%s:%s"
 )
 
 func Deploy(module, version string, mem int) {
 	serviceName := strings.Replace(module, "/", "--", 1)
-	fmt.Printf(deployTemplate+"\n", serviceName, mem, mem, module, version, module, version)
+	fmt.Printf(deployTemplate+"\n", serviceName, mem, mem, module, version, ClusterAddr, ClusterAddr, ClusterAddr, module, version)
 }
