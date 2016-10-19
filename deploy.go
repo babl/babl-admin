844dbf6e5e56d696ef826d333d96ba2967d81359
Lars Kluge <l@larskluge.com>
2016-10-19 11:12:43 +0200

    Do not use deprecated modules network

--- deploy.go
+++ deploy.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	deployTemplate = "docker service create --name %s --label=module --network=modules -p 4444 --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=%s:9092 -e BABL_STORAGE=%s:4443 -e BABL_ENDPOINT=%s:4445 -e BABL_COMMAND_TIMEOUT=%s registry.babl.sh/%s:%s"
+	deployTemplate = "docker service create --name %s --label=module --log-driver=journald --reserve-memory=%dM --limit-memory=%dM -e BABL_MODULE=%s -e BABL_MODULE_VERSION=%s -e BABL_COMMAND=/bin/app -e BABL_KAFKA_BROKERS=%s:9092 -e BABL_STORAGE=%s:4443 -e BABL_ENDPOINT=%s:4445 -e BABL_COMMAND_TIMEOUT=%s registry.babl.sh/%s:%s"
 )
 
 func Deploy(module, version string, mem int, timeout string) {
